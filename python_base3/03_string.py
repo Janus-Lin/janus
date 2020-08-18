14f941293c651a70ff80615788616c0a3e2b1473
lin <lin@moneyworks.cn>
2020-08-18 08:59:15 +0800

    添加字典, 相关知识

--- python_base3/03_string.py
+++ python_base3/03_string.py
@@ -1,8 +1,70 @@
 """
-janus.03_string
-~~~~~~~~~~~~~~~~~~~~~~~
+janus.python_base3.03_string
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-功能:
+功能: 字符串
 
-版权所有 © 2020, 美通智投（北京）科技有限公司
+版权所有 © 2020,
 """
+import string
+
+
+# ------------------------模块string中几个很有用的常量------------------------------
+# 包含数字0～9的字符串
+print(string.digits)
+
+# 包含所有ASCII字母（大写和小写）的字符串
+print(string.ascii_letters)
+
+# 包含所有小写ASCII字母的字符串
+print(string.ascii_lowercase)
+
+# 包含所有可打印的ASCII字符的字符串
+print(string.printable)
+
+# 包含所有ASCII标点字符的字符串
+print(string.punctuation)
+
+# 包含所有大写ASCII字母的字符串
+print(string.ascii_uppercase)
+
+# -------------------------------字符串方法---------------------------------------
+#  center: 两边添加填充字符（默认为空格）让字符串居中'i love you'.center(2, "*")
+print('i love you'.center(39, "*"))
+
+# find：在字符串中查找子串。如果找到，就返回子串的第一个字符的索引，否则返回-1。
+# 附录B：rfind、index、rindex、count、startswith、endswith。
+title = "Monty Python's Flying Circus"
+print(title.find('Python'))
+
+# join： 是一个非常重要的字符串方法，其作用与split相反，用于合并序列的元素。
+seq = ['1', '2', '3', '4', '5']
+print('-'.join(seq))
+
+# lower: 返回字符串的小写版本。解决方案是在存储和搜索时
+# islower、istitle、isupper、translate, capitalize、casefold、swapcase、title、upper
+print('Trondheim'.lower())
+print("that's all, folks".title()) # 首字母大写
+print(string.capwords("that's all, folks")) # 更好点
+
+# replace： 将指定子串都替换为另一个字符串，并返回替换后的结果
+print('This is a test'.replace('is', 'eez') )
+
+# split： 一个非常重要的字符串方法，其作用与join相反，用于将字符串拆分为序列。
+# partition、rpartition、rsplit、splitlines
+print( '1+2+3+4+5'.split('+') )
+
+# strip： 将字符串开头和末尾的空白（但不包括中间的空白）删除，并返回删除后的结果。
+# 应用场景：用户输入用户名时不小心在末尾加上了一个空格
+print(' internal whitespace is kept '.strip())
+print('×××internal whitespace is kept×××'.strip("×"))
+
+# translate： 与replace一样替换字符串的特定部分，但不同的是它只能进行单字符替换。这个方法的优势在于能够同时替换多个字符，因此效率比replace高
+# 使用translate前必须创建一个转换表，长度相同的字符串，对应替换。,提供可选的第三个参数指定要将哪些字母删除
+table = str.maketrans('cs', 'kz')
+print('this is an incredible test'.translate(table))
+table = str.maketrans('cs', 'kz', ' ')
+print('this is an incredible test'.translate(table))
+
+# 判断字符串是否满足特定的条件
+# isalnum、isalpha、isdecimal、isdigit、isidentifier、islower、isnumeric、isprintable、isspace、istitle、isupper。
---
+++ python_base3/04_dict.py
@@ -0,0 +1,99 @@
+"""
+janus.python_base3.04_dict
+~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+功能: 字典
+
+版权所有 © 2020
+"""
+
+"""
+字典： 通过名称来访问其各个值的数据结构。这种数据结构称为映射（mapping）。
+字典是Python中唯的内置映射类型，其中的值不按顺序排列，而是存储在键下。键可能是数、字符串或元组。
+dict其实根本就不是函数，而是一个类
+"""
+phonebook = {'Alice': '2341', 'Beth': '9102', 'Cecil': '3258'}
+
+items = [('name', 'Gumby'), ('age', 42)]
+d = dict(items)
+d = dict(name='Gumby', age=42)
+
+# 字典的基本行为在很多方面都类似于序列。
+len(d)  # 返回字典d包含的项（键值对） d[k]返回与键k相关联的值。
+d['k'] = 'v'  # 将值v关联到键k。
+del d['k']  # 删除键为k的项。
+print('k' in d)  # 检查字典d是否包含键为k的项。
+
+# 注意：
+"""
+键的类型：字典中的键可以是整数，但并非必须是整数
+自动添加：即便是字典中原本没有的键，也可以给它赋值，这将在字典中创建一个然而，如果不使用append或其他类似的方法，就不能给列表中没有的元素赋值。
+成员资格：表达式k in d（其中d是一个字典）查找的是键而不是值，而表达式v in l（其中l是一个列表）查找的是值而不是索引。检查字典是否包含指定的键的效率更高
+"""
+
+# 字典方法
+# clear: 删除所有的字典项，这种操作是就地执行的（就像list.sort一样），因此什么都不返回（或者说返回None）
+d.clear()
+
+# copy: 返回一个新字典，其包含的键值对与原来的字典相同（这个方法执行的是浅复制，因为值本身是原件，而非副本）
+# 当替换副本中的值时，原件不受影响。然而，如果修改副本中的值（就地修改而不是替换），原件也将发生变化，因为原件指向的也是被修改的值
+x = {'username': 'admin', 'machines': ['foo', 'bar', 'baz']}
+y = x.copy()
+y['username'] = 'mlh'
+y['machines'].remove('bar')
+print(x, y)
+
+# 为避免这种问题，一种办法是执行深复制，即同时复制值及其包含的所有值，等等。为此，可使用模块copy中的函数deepcopy
+from copy import deepcopy
+
+d = {}
+d['names'] = ['Alfred', 'Bertrand']
+c = d.copy()
+dc = deepcopy(d)
+d['names'].append('Clive')
+print(x, y)
+
+# fromkeys: 创建一个新字典，其中包含指定的键，且每个键对应的值都是None。
+a = {}.fromkeys(['name', 'age'])
+print(a)
+print(dict.fromkeys(['name', 'age']))
+print(dict.fromkeys(['name', 'age'], '(unknown)'))  # 不想使用默认值None，可提供特定的值
+
+# get： 为访问字典项提供了宽松的环境。通常，如果你试图访 d.get('name', 'N/A')问字典中没有的项，将引发错误
+print(d.get('name'))  # d['name']会报错
+print(d.get('name', 'N/A'))  # key不存在返回特定值
+
+# items： 返回一个包含所有字典项的列表，其中每个元素都为(key, value)的形式。字典项在列表中的排列顺序不确定
+# 返回值属于一种名为字典视图的特殊类型。字典视图可用于迭代
+d = {'title': 'Python Web Site', 'url': 'http://www.python.org', 'spam': 0}
+print(d.items())
+
+# keys: 返回一个字典视图，其中包含指定字典中的键。
+print(d.keys())
+
+# pop: 可用于获取与指定键相关联的值，并将该键值对从字典中删除
+d.pop('title')
+print(d)
+
+# popitem: 类似于list.pop，但list.pop弹出列表中的最后一个元素，而popitem随机地弹出一个字典项，因为字典项的顺序是不确定的，没有“最后一个元素”的概念
+d.popitem()
+print(d)
+
+# 指定的键不存在时，setdefault返回指定的值并相应地更新字典。如果指定的键存在，就返回其值，并保持字典不变。与get一样，值是可选的；如果没有指定，默认为None。
+d = {}
+d.setdefault('name', 'N/A')
+print(d)
+
+# update: 使用一个字典中的项来更新另一个字典
+y = {
+    'title': 'Python Web Site',
+    'url': 'http://www.python.org',
+    'changed': 'Mar 14 22:09:15 MET 2016'
+}
+x = {'title': 'Python Language Website'}
+y.update(x)
+print(y)
+
+# values: 返回一个由字典中的值组成的字典视图。不同于方法keys，方法values返回的图可能包含重复的值
+d = {1: 1, 2: 2, 3: 3, 4: 1}
+print(d.values())
